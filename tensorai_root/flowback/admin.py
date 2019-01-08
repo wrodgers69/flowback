b0d4f3e46562e4d98a90e0370ec2ce07c81a3dda
Lee Rodgers <leerrodgers@gmail.com>
2019-01-08 01:10:05 -0600

    Updating Dockerfile and docker-compose to allow for better development experience

--- tensorai_root/flowback/admin.py
+++ tensorai_root/flowback/admin.py
@@ -1,8 +1,2 @@
-from django.contrib import admin
-
 # Register your models here.
 from django.contrib import admin
-from .models import Well_Profile, Well_Data
-
-admin.site.register(Well_Profile)
-admin.site.register(Well_Data)
